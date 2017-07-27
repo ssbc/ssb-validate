12f4c48a7715d8cbc6f07e80138df6d05dd81cd6
Dominic Tarr <dominic.tarr@gmail.com>
2017-07-27 21:32:05 +1200

    move logic from dump.js into async.js

--- dump.js
+++ dump.js
@@ -1,19 +1,34 @@
 var validate = require('./')
 var pull = require('pull-stream')
-var state = {
-  queue: [],
-  feeds: {},
-  error: null
-}
-require('ssb-client')(function (err, sbot) {
-  var c = 0, _c = 0, e = 0, d = 0, q = 0, ts = Date.now(), start = Date.now()
+var state = validate.initial()
 
-  function log () {
-    if(Date.now() > ts + 1000) {
+var Log = require('flumelog-offset')
+
+var logfile = '/tmp/ssb-validate_dump-test'
+try { require('fs').unlinkSync(logfile) } catch (_) {}
+
+var log = Log(logfile, 1024, require('flumecodec/json'))
+console.log(log)
+
+var async = require('./async')(state, log)
+
+require('ssb-client')(function (err, sbot) {
+  var c = 0, _c = 0, e = 0, d = 0, q = 0, v = 0, ts = Date.now(), start = Date.now()
+
+  console.log('c, _c, d, e, q, v, c/s, mb, mb/s, s')
+
+  function ConsoleLog () {
       var s = (ts - start)/1000
-      console.log(c, c - _c, d, e, q, c / s, s)
+      var mb = log.since.value / (1024*1024)
+      console.log(c, c - _c, d, e, q, v, c / s, mb, mb/s, s)
       _c = c
       d = 0
+
+  }
+
+  function maybeLog () {
+    if(Date.now() > ts + 1000) {
+      ConsoleLog()
       ts = Date.now()
     }
   }
@@ -21,40 +36,51 @@ require('ssb-client')(function (err, sbot) {
   var author = null
 
   pull(
-    sbot.createLogStream(),
-    pull.drain(function (data) {
-      if(author && data.value.author != author && state.feeds[author].queue.length > 20) {
-        q -= state.feeds[author].queue.length
-        state = validate.validate(state, author)
-        state = validate.queue(state, data.value)
-        q++
+    sbot.createLogStream({}),
+    function (read) {
+      return function (abort, cb) {
+        read(abort, function next (err, data) {
+          q = async.queued()
+          v = async.validated()
+          c = q + v
+          if(err) {
+            ConsoleLog()
+            return async.flush(true, function (_) {
+              cb(err)
+            })
+          }
+          maybeLog()
+          async.queue(data.value)
+          if(state.queue.length > 100) {
+            async.flush(null, function (err, data) {
+              cb(err, data)
+            })
+          }
+          else
+            read(null, next)
+        })
       }
-      else {
-        q++
-        state = validate.queue(state, data.value)
-      }
-
-      author = data.value.author
-
-      if(state.error) {
-        e++
-        console.log(state.error)
-        state.error = null
-      }
-      else if(state.queue.length)
-        d += JSON.stringify(state.queue.shift(), null, 2).length //can gc it
-      log()
-      c++
-    }, function () {
+  },
+  pull.drain(null, function () {
+      var l
+      ConsoleLog()
       for(var k in state.feeds) {
-        if(state.feeds[k].queue.length)
+        if(l = state.feeds[k].queue.length) {
           validate.validate(state, k)
-        log()
+          q -= l
+          v += l
+          maybeLog()
+        }
       }
+      console.log('validate', k, q, v)
+      ConsoleLog()
+      sbot.close()
     })
   )
 
 })
+
+
 
 
 
