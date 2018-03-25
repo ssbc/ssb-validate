c8c70f89b79b6049b43daa63e6628477e940ecf1
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-26 10:01:23 +1300

    add all the tests I can think of, everything depends on this so it needs tests. (I'm a bit surprised that something didn't catch this already.. @happy0 @noffle @mixmix)

--- test/error.js
+++ test/error.js
@@ -1,8 +1,9 @@
 var tape = require('tape')
 var ssbKeys = require('ssb-keys')
+var crypto = require('crypto')
 
 function hash (seed) {
-  return require('crypto').createHash('sha256').update(seed).digest()
+  return crypto.createHash('sha256').update(seed).digest()
 }
 
 var keys = ssbKeys.generate('ed25519', hash('validation-test-seed1'))
@@ -98,11 +99,122 @@ function test (hmac_key) {
     t.end()
   })
 
+  var ctxt = crypto.randomBytes(1024).toString('base64')+'.box'
+
+  tape('create with invalid content', function (t) {
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, null, Date.now())
+    })
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, true, Date.now())
+    })
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, false, Date.now())
+    })
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, 0, Date.now())
+    })
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, 100, Date.now())
+    })
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, [], Date.now())
+    }, 'array as content')
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, new Buffer('hello'), Date.now())
+    }, 'buffer as content')
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, new Date(), Date.now())
+    }, 'date as content')
+
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {}, Date.now())
+    })
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {tyfe:'not-okay' }, Date.now())
+    })
+
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {tyfe:'not-okay' }, Date.now())
+    })
+
+    //type too long
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {type:ctxt }, Date.now())
+    })
+
+    //type too long
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {type:keys.id }, Date.now())
+    })
+    // type too short
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {type:'T' }, Date.now())
+    })
+    // type too short
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, {type:'TT' }, Date.now())
+    })
+    // content too long
+    t.throws(function () {
+      var m = v.create(null, keys, hmac_key, crypto.randomBytes(8*1024).toString('base64')+'.box', Date.now())
+    }, 'content too long')
+    t.end()
+  })
+   
+  tape('valid messages', function (t) {
+    var msg
+
+    //type must be 3 chars
+    var msg = v.create(null, keys, hmac_key, {type:'TTT' }, Date.now())
+    t.deepEqual(msg.content, {type: 'TTT'})
+
+    //type can be msg id
+    var msg_id = '%'+crypto.randomBytes(32).toString('base64')+'.sha256'
+    msg = v.create(null, keys2, hmac_key, {type: msg_id}, +new Date('2017-04-11 9:09 UTC'))
+    t.deepEqual(msg.content, {type: msg_id})
+
+    //content can be encrypted.
+    msg = v.create(null, keys2, hmac_key, ctxt, +new Date('2017-04-11 9:09 UTC'))
+    t.equal(msg.content, ctxt)
+    //content can be encrypted.
+
+    //content encrypted with future private box version
+    msg = v.create(null, keys2, hmac_key, ctxt+'2', +new Date('2017-04-11 9:09 UTC'))
+    t.equal(msg.content, ctxt+'2')
+
+    msg = v.create(null, keys, hmac_key, {type: 'okay'}, Date.now())
+    t.deepEqual(msg.content, {type: 'okay'})
+
+    //for backwards compatibilty reasons, we only count the
+    //javascript string length of a message, so it may actually
+    //be encoded as longer that 8k, if it uses unicode.
+
+    //so a message with 7000 euro signs is valid, even though
+    //it is technically longer than we intended.
+
+    //at some point, we'll introduce a binary encoding for
+    //messages and we'll fix this then.
+    var text = ''
+    for(var i = 0; i < 7000; i++)
+      text += '\u20ac'
+    t.ok(text.length < 8124)
+    console.log(text.length)
+    console.log(new Buffer(JSON.stringify({text: text}), 'utf8').length)
+    t.ok(new Buffer(JSON.stringify({text: text}), 'utf8').length > 8000)
+    msg = v.create(null, keys, hmac_key, {type: 'euros', text: text}, Date.now())
+    t.deepEqual(msg.content, {type: 'euros', text: text})
+
+    t.end()
+  })
+
 }
 
 test()
 test(hash('hmac_key'))
 test(hash('hmac_key2'))
+
+
 
 
 
